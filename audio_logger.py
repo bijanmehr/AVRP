d499555661977630429fb7970bc14e8c7a474b5e
BMH <bijanmehralizadeh@gmail.com>
2019-02-18 14:20:52 +0330

    audio recorder class
    init

---
+++ audio_logger.py
@@ -0,0 +1,83 @@
+import pyaudio
+import wave
+import os
+import time
+import threading
+import csv
+
+
+class Recorder():
+    #Defines sound properties like frequency and channels
+    def __init__(self, chunk=1024, channels=2, rate=44100):
+        self.CHUNK = chunk
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = channels
+        self.RATE = rate
+        self._running = True
+        self._frames = []
+        self.input_device_index = 4
+
+
+    def dev(self):
+        p = pyaudio.PyAudio()
+        info = p.get_host_api_info_by_index(0)
+        numdevices = info.get('deviceCount')
+        for i in range(0, numdevices):
+                if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+                    print ("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+    #Start recording sound
+    def start(self):
+        threading._start_new_thread(self.__recording, ())
+
+    def __recording(self):
+        #Set running to True and reset previously recorded frames
+        self._running = True
+        self._frames = []
+        #Create pyaudio instance
+        p = pyaudio.PyAudio()
+        #Open stream
+        stream = p.open(format=self.FORMAT,
+                        channels=self.CHANNELS,
+                        rate=self.RATE,
+                        input=True,
+                        frames_per_buffer=self.CHUNK)
+        # To stop the streaming, new thread has to set self._running to false
+        # append frames array while recording
+        while(self._running):
+            data = stream.read(self.CHUNK)
+            self._frames.append(data)
+
+        # Interrupted, stop stream and close it. Terinate pyaudio process.
+        stream.stop_stream()
+        stream.close()
+        p.terminate()
+
+    # Sets boolean to false. New thread needed.
+    def stop(self):
+        self._running = False
+
+    #Save file to filename location as a wavefront file.
+    def save(self, filename):
+        print("Saving")
+        p = pyaudio.PyAudio()
+        if not filename.endswith(".wav"):
+            filename = filename + ".wav"
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(self.CHANNELS)
+        wf.setsampwidth(p.get_sample_size(self.FORMAT))
+        wf.setframerate(self.RATE)
+        wf.writeframes(b''.join(self._frames))
+        wf.close()
+        print("Saved")
+
+
+if __name__ == "__main__":
+    rec = Recorder()
+    print("Start recording")
+    rec.start()
+    time.sleep(5)
+    print("Stop recording")
+    rec.stop()
+    print("Saving")
+    rec.save("test.wav")
